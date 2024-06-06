ddb55912e55b8f88601d66744c5be331921b745f
Pikatsuto <pikatsuto@gmail.com>
2024-06-06 21:27:51 +0200

    fix: src path

--- pkgs/by-name/rofi-beats/package.nix
+++ pkgs/by-name/rofi-beats/package.nix
@@ -20,11 +20,7 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "rofi-beats";
   version = "24.05-06-06-2024";
   # ----------------------------------------------------------------- #
-  src = fetchurl {
-    url = "https://github.com/NixAchu/rofi-beats/releases/download/1.0.0/rofi-beats.tar.gz";
-    hash = "sha256-t6CKpfuzqZmmEmW/BpNShH2+bCQIAogbnEBgXdjVYvI=";
-  };
-  sourceRoot = ".";
+  src = ./src; 
   # ----------------------------------------------------------------- #
   nativeBuildInputs = [ makeWrapper ];
   # ----------------------------------------------------------------- #
