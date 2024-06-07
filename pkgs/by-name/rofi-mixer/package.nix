78ec1ce79c123a93e6c52499f4ef47993e136075
Pikatsuto <pikatsuto@gmail.com>
2024-06-07 14:14:04 +0200

    feat: add rofi-mixer package

---
+++ pkgs/by-name/rofi-mixer/package.nix
@@ -0,0 +1,67 @@
+{
+  stdenv,
+  lib,
+  callPackage,
+  makeWrapper,
+  rofi-wayland,
+}:
+############
+# Packages #
+#######################################################################
+let
+  iconPath = "icon.png";
+  name = "Rofi Mixer";
+  comment = "Rofi sound out/in manager";
+in
+# ----------------------------------------------------------------- #
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rofi-mixer";
+  version = "24.05-06-06-2024";
+  src = ./src; 
+  # ----------------------------------------------------------------- #
+  nativeBuildInputs = [ makeWrapper ];
+  prePatch = ''
+    patchShebangs . ;
+  '';
+  # ----------------------------------------------------------------- #
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    echo -e "[Desktop Entry]\n" \
+      "Type=Application\n" \
+      "Name=${name}\n" \
+      "Comment=${comment}\n" \
+      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
+      "Exec=$out/bin/${finalAttrs.pname}\n" \
+      "Terminal=false" > ./${finalAttrs.pname}.desktop
+
+    install -D ${finalAttrs.pname}.desktop \
+      $out/share/applications/${finalAttrs.pname}.desktop
+
+    runHook postInstall
+  '';
+  # ----------------------------------------------------------------- #
+  postFixup = ''
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix PATH : ${lib.makeBinPath [
+        rofi-wayland
+        (callPackage ../focus-rofi/package.nix { })
+      ]}
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = comment;
+    homepage = "https://github.com/nickclyde/rofi-bluetooth";
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
+
