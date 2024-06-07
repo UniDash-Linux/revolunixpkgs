d6a8add86b95336de2123225d3b814a61fe93c88
Pikatsuto <pikatsuto@gmail.com>
2024-06-07 14:08:29 +0200

    feat: add focus-rofi package

---
+++ pkgs/by-name/focus-rofi/package.nix
@@ -0,0 +1,38 @@
+{
+  stdenv,
+  lib,
+}:
+############
+# Packages #
+#######################################################################
+stdenv.mkDerivation (finalAttrs: {
+  pname = "focus-rofi";
+  version = "24.05-06-06-2024";
+  src = ./src; 
+  # ----------------------------------------------------------------- #
+  prePatch = ''
+    patchShebangs . ;
+  '';
+  # ----------------------------------------------------------------- #
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    runHook postInstall
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = "Rofi focus fixer";
+    homepage = "https://github.com/nickclyde/rofi-bluetooth";
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
+
