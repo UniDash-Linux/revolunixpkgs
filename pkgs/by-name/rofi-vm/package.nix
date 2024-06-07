c4d9aaf3a817fc7d303a4b0ba07cfc9e97034f38
Pikatsuto <pikatsuto@gmail.com>
2024-06-07 13:24:33 +0200

    fix: rofi-vm theme error

--- pkgs/by-name/rofi-vm/package.nix
+++ pkgs/by-name/rofi-vm/package.nix
@@ -1,26 +1,63 @@
-{ stdenv, lib }:
+{
+  stdenv,
+  lib,
+  makeWrapper,
+  rofi-wayland,
+}:
 ############
 # Packages #
 #######################################################################
-stdenv.mkDerivation {
+let
+  iconPath = "icon.png";
+  name = "Rofi Virtual Machine";
+  comment = "Rofi virtual machine manager";
+in
+# ----------------------------------------------------------------- #
+stdenv.mkDerivation (finalAttrs: {
   pname = "rofi-vm";
-  version = "unstable-2023-07-16";
+  version = "24.05-06-06-2024";
   # ----------------------------------------------------------------- #
-  src = ./src;
+  src = ./src; 
+  # ----------------------------------------------------------------- #
+  nativeBuildInputs = [ makeWrapper ];
+  # ----------------------------------------------------------------- #
+  prePatch = ''
+    patchShebangs . ;
+  '';
   # ----------------------------------------------------------------- #
   installPhase = ''
     runHook preInstall
 
-    install -Dm 755 rofi-vm $out/bin/rofi-vm
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    echo -e "[Desktop Entry]\n" \
+      "Type=Application\n" \
+      "Name=${name}\n" \
+      "Comment=${comment}\n" \
+      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
+      "Exec=$out/bin/${finalAttrs.pname}\n" \
+      "Terminal=false" > ./${finalAttrs.pname}.desktop
+
+    install -D ${finalAttrs.pname}.desktop \
+      $out/share/applications/${finalAttrs.pname}.desktop
 
     runHook postInstall
   '';
   # ----------------------------------------------------------------- #
-  meta = with lib; {
-    description = "rofi VM manager";
-    maintainers = [ maintainers.pikatsuto ];
-    licenses = licenses.gpl3;
-    platforms = platforms.linux;
+  postFixup = ''
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix PATH : ${lib.makeBinPath [ rofi-wayland ]}
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = comment;
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
   };
-#######################################################################
-}
+  #######################################################################
+})
