25be15063943310d8be301c42886321708d1532c
Pikatsuto <pikatsuto@gmail.com>
2024-06-07 13:31:23 +0200

    feat: add rofi-wpa package

---
+++ pkgs/by-name/rofi-wpa/package.nix
@@ -0,0 +1,63 @@
+{
+  stdenv,
+  lib,
+  makeWrapper,
+  rofi-wayland,
+}:
+############
+# Packages #
+#######################################################################
+let
+  iconPath = "icon.png";
+  name = "Rofi WPA Supplicant";
+  comment = "Rofi WIFI manager";
+in
+# ----------------------------------------------------------------- #
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rofi-wpa";
+  version = "24.05-06-06-2024";
+  # ----------------------------------------------------------------- #
+  src = ./src; 
+  # ----------------------------------------------------------------- #
+  nativeBuildInputs = [ makeWrapper ];
+  # ----------------------------------------------------------------- #
+  prePatch = ''
+    patchShebangs . ;
+  '';
+  # ----------------------------------------------------------------- #
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    echo -e "[Desktop Entry]\n" \
+      "Type=Application\n" \
+      "Name=${name}\n" \
+      "Comment=${comment}\n" \
+      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
+      "Exec=$out/bin/${finalAttrs.pname}\n" \
+      "Terminal=false" > ./${finalAttrs.pname}.desktop
+
+    install -D ${finalAttrs.pname}.desktop \
+      $out/share/applications/${finalAttrs.pname}.desktop
+
+    runHook postInstall
+  '';
+  # ----------------------------------------------------------------- #
+  postFixup = ''
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix PATH : ${lib.makeBinPath [ rofi-wayland ]}
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = comment;
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
