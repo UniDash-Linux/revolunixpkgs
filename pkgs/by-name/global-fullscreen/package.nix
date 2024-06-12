41a3835d543d749404231a8d66f1635281b63c8d
Pikatsuto <pikatsuto@gmail.com>
2024-06-13 01:29:50 +0200

    feat: add global-fullscreen package

---
+++ pkgs/by-name/global-fullscreen/package.nix
@@ -0,0 +1,44 @@
+{
+  stdenv,
+  lib,
+  makeWrapper,
+  waybar,
+}:
+############
+# Packages #
+#######################################################################
+stdenv.mkDerivation (finalAttrs: {
+  pname = "global-fullscreen";
+  version = "24.05-13-06-2024";
+  src = ./src; 
+  # ----------------------------------------------------------------- #
+  nativeBuildInputs = [ makeWrapper ];
+  prePatch = ''
+    patchShebangs . ;
+  '';
+  # ----------------------------------------------------------------- #
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    runHook postInstall
+  '';
+  # ----------------------------------------------------------------- #
+  postFixup = ''
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix PATH : ${lib.makeBinPath [ waybar ]}
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = "CLI backup/sync manager";
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.lgpl;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
+
