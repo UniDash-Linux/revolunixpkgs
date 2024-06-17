a70b38446b9f0584d4ea65d4e8eaebcdcbf53ee7
Pikatsuto <pikatsuto@gmail.com>
2024-06-17 18:12:05 +0000

    feat: Update/Upload focus-rofi package

--- pkgs/focus-rofi/package.nix
+++ pkgs/focus-rofi/package.nix
@@ -2,36 +2,22 @@
   fetchurl,
   stdenv,
   lib,
-  makeWrapper,
-  bash,
 }:
 ############
 # Packages #
-#########################################################################
-let
-  iconPath = "icon.png";
-  name = "Exemple Application";
-  comment = "Exemple Application";
-in
-# --------------------------------------------------------------------- #
+#######################################################################
 stdenv.mkDerivation (finalAttrs: {
-  pname = "exemple";
-  version = "nightly-2024.06.17-18.02.13";
-  ## ----------------------------------------------------------------- ##
+  pname = "focus-rofi";
+  version = "nightly-2024.06.17-18.12.02";
   src = fetchurl {
-    url = "https://github.com/RevoluNix/pkg-focus-rofi/releases/download/nightly-2024.06.17-18.02.13/src-focus-rofi.tar.gz";
-    sha256 = "72ecb42e8251cd902d772b2261c0cc78d33c67ba05c89b958b76d353a05692b6";
+    url = "https://github.com/RevoluNix/pkg-focus-rofi/releases/download/nightly-2024.06.17-18.12.02/src-focus-rofi.tar.gz";
+    sha256 = "ed9b0cbca4b9ecbd43d15d7cba5b40fcc60423c668812cc442db0d94f7f5fea4";
   }; 
-  ## ----------------------------------------------------------------- ##
-  nativeBuildInputs = [ makeWrapper ];
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   prePatch = ''
     patchShebangs . ;
-
-    substituteInPlace exemple \
-      --replace-fail "exemple-2" "${placeholder "out"}/bin/exemple-2"
   '';
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   installPhase = ''
     runHook preInstall
 
@@ -39,36 +25,17 @@ stdenv.mkDerivation (finalAttrs: {
     cp -r ./ $out/Applications/${finalAttrs.pname}/
 
     install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
-    install -Dm 755 exemple-2 $out/bin/exemple-2
-
-    echo -e "[Desktop Entry]\n" \
-      "Type=Application\n" \
-      "Name=${name}\n" \
-      "Comment=${comment}\n" \
-      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
-      "Exec=$out/bin/${finalAttrs.pname}\n" \
-      "Terminal=false" > ./${finalAttrs.pname}.desktop
-
-    install -D ${finalAttrs.pname}.desktop \
-      $out/share/applications/${finalAttrs.pname}.desktop
 
     runHook postInstall
   '';
-  ## ----------------------------------------------------------------- ##
-  postFixup = ''
-    wrapProgram $out/bin/exemple-2 \
-      --prefix PATH : ${lib.makeBinPath [
-        bash
-      ]}
-  '';
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   meta = {
-    description = comment;
-    homepage = "https://github.com/RevoluNix/pkgs-template/";
+    description = "Rofi focus fixer";
     maintainers = with lib.maintainers; [ pikatsuto ];
-    licenses = lib.licenses.lgpl2;
+    licenses = lib.licenses.gpl3Plus;
     platforms = lib.platforms.linux;
     mainProgram = finalAttrs.pname;
   };
   #######################################################################
 })
+
