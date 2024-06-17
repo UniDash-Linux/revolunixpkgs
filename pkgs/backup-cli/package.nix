e1650542bd93c9d9faa527553e4c29932292d3c7
Pikatsuto <pikatsuto@gmail.com>
2024-06-17 21:31:56 +0000

    feat: Update/Upload backup-cli package

---
+++ pkgs/backup-cli/package.nix
@@ -0,0 +1,74 @@
+{
+  fetchurl,
+  stdenv,
+  lib,
+  makeWrapper,
+  bash,
+}:
+############
+# Packages #
+#########################################################################
+let
+  iconPath = "icon.png";
+  name = "Exemple Application";
+  comment = "Exemple Application";
+in
+# --------------------------------------------------------------------- #
+stdenv.mkDerivation (finalAttrs: {
+  pname = "exemple";
+  version = "release-2024.06.17-21.31.53";
+  ## ----------------------------------------------------------------- ##
+  src = fetchurl {
+    url = "https://github.com/RevoluNix/backup-cli/releases/download/release-2024.06.17-21.31.53/src-backup-cli.tar.gz";
+    sha256 = "87c0a15d3e909bc303976cfc2cf429b1f4bdc2d6c5081a3975e6d55eef6eee32";
+  }; 
+  ## ----------------------------------------------------------------- ##
+  nativeBuildInputs = [ makeWrapper ];
+  ## ----------------------------------------------------------------- ##
+  prePatch = ''
+    patchShebangs . ;
+
+    substituteInPlace exemple \
+      --replace-fail "exemple-2" "${placeholder "out"}/bin/exemple-2"
+  '';
+  ## ----------------------------------------------------------------- ##
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+    install -Dm 755 exemple-2 $out/bin/exemple-2
+
+    echo -e "[Desktop Entry]\n" \
+      "Type=Application\n" \
+      "Name=${name}\n" \
+      "Comment=${comment}\n" \
+      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
+      "Exec=$out/bin/${finalAttrs.pname}\n" \
+      "Terminal=false" > ./${finalAttrs.pname}.desktop
+
+    install -D ${finalAttrs.pname}.desktop \
+      $out/share/applications/${finalAttrs.pname}.desktop
+
+    runHook postInstall
+  '';
+  ## ----------------------------------------------------------------- ##
+  postFixup = ''
+    wrapProgram $out/bin/exemple-2 \
+      --prefix PATH : ${lib.makeBinPath [
+        bash
+      ]}
+  '';
+  ## ----------------------------------------------------------------- ##
+  meta = {
+    description = comment;
+    homepage = "https://github.com/RevoluNix/pkgs-template/";
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.lgpl2;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
