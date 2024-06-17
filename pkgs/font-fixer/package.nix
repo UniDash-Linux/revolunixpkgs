51048da1b4fdb91c0dc62eb3abdda9ac69b668a7
Pikatsuto <pikatsuto@gmail.com>
2024-06-17 23:11:10 +0000

    feat: Update/Upload font-fixer package

---
+++ pkgs/font-fixer/package.nix
@@ -0,0 +1,58 @@
+{
+  fetchurl,
+  stdenv,
+  lib,
+}:
+############
+# Packages #
+#######################################################################
+let
+  iconPath = "icon.png";
+  name = "Font Fixer";
+  comment = "Copy store font to user path";
+in
+# ----------------------------------------------------------------- #
+stdenv.mkDerivation (finalAttrs: {
+  pname = "font-fixer";
+  version = "release-2024.06.17-23.11.08";
+  # ----------------------------------------------------------------- #
+  src = fetchurl {
+    url = "https://github.com/RevoluNix/pkg-font-fixer/releases/download/release-2024.06.17-23.11.08/src-font-fixer.tar.gz";
+    sha256 = "449104a7427c9f0bc703d5ec44637f06f8359162e88a426400dc667a6af86ddc";
+  }; 
+  # ----------------------------------------------------------------- #
+  prePatch = ''
+    patchShebangs . ;
+  '';
+  # ----------------------------------------------------------------- #
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    echo -e "[Desktop Entry]\n" \
+      "Type=Application\n" \
+      "Name=${name}\n" \
+      "Comment=${comment}\n" \
+      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
+      "Exec=$out/bin/${finalAttrs.pname}\n" \
+      "Terminal=false" > ./${finalAttrs.pname}.desktop
+
+    install -D ${finalAttrs.pname}.desktop \
+      $out/share/applications/${finalAttrs.pname}.desktop
+
+    runHook postInstall
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = comment;
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
