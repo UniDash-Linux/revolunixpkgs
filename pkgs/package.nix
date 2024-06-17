be4f4d9eea2889f0953445291c5ff179b2fef075
Pikatsuto <pikatsuto@gmail.com>
2024-06-17 21:26:34 +0000

    feat: Update/Upload  package

--- pkgs/package.nix
+++ pkgs/package.nix
@@ -1,25 +1,32 @@
 {
   stdenv,
   lib,
-  callPackage,
+  fetchurl,
   makeWrapper,
-  rofi-wayland,
+  appimage-run,
 }:
 ############
 # Packages #
 #######################################################################
 let
-  iconPath = "icon.png";
-  name = "Rofi Mixer";
-  comment = "Rofi sound out/in manager";
+  appimageName = "citra-qt.AppImage";
+  iconPath = "dist/citra.png";
+  name = "Citra PabloMK7";
+  comment = "Citra 3DS emulator";
 in
 # ----------------------------------------------------------------- #
 stdenv.mkDerivation (finalAttrs: {
-  pname = "rofi-mixer";
-  version = "24.05-06-06-2024";
-  src = ./src; 
+  pname = "citra";
+  version = "nightly-2024.06.17-21.26.32";
+  # ----------------------------------------------------------------- #
+  src = fetchurl {
+    url = "https://github.com/PabloMK7/citra/releases/download/r4f174f1/citra-linux-appimage-20240615-4f174f1.tar.gz";
+    sha256 = "49702ca01a5c91431428489c0567437d5161bb19002e36f48e6836c6e7fcad62";
+  };
+  sourceRoot = ".";
   # ----------------------------------------------------------------- #
   nativeBuildInputs = [ makeWrapper ];
+  # ----------------------------------------------------------------- #
   prePatch = ''
     patchShebangs . ;
   '';
@@ -27,9 +34,12 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/bin/ $out/Applications/
-    cp -r ./ $out/Applications/${finalAttrs.pname}/
+    mkdir -p $out/Applications
+    cp -r ./*/ $out/Applications/${finalAttrs.pname}
 
+    echo -e "#!" "/usr/bin/env sh\n\n" \
+      "appimage-run $out/Applications/${finalAttrs.pname}/${appimageName}" \
+      > ./${finalAttrs.pname}
     install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
 
     echo -e "[Desktop Entry]\n" \
@@ -48,19 +58,16 @@ stdenv.mkDerivation (finalAttrs: {
   # ----------------------------------------------------------------- #
   postFixup = ''
     wrapProgram $out/bin/${finalAttrs.pname} \
-      --prefix PATH : ${lib.makeBinPath [
-        rofi-wayland
-        (callPackage ../focus-rofi/package.nix { })
-      ]}
+      --prefix PATH : ${lib.makeBinPath [ appimage-run ]}
   '';
   # ----------------------------------------------------------------- #
   meta = {
     description = comment;
+    homepage = "https://github.com/PabloMK7/citra";
     maintainers = with lib.maintainers; [ pikatsuto ];
-    licenses = lib.licenses.gpl3Plus;
+    licenses = lib.licenses.gpl2;
     platforms = lib.platforms.linux;
     mainProgram = finalAttrs.pname;
   };
   #######################################################################
 })
-
