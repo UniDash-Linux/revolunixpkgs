cd4a33e00efc27585b7d9fc56c0b7485de28cbf8
Pikatsuto <pikatsuto@gmail.com>
2024-06-17 23:07:41 +0000

    feat: Update/Upload backup-cli package

---
+++ pkgs/backup-cli/package.nix
@@ -0,0 +1,48 @@
+{
+  fetchurl,
+  stdenv,
+  lib,
+  makeWrapper,
+  rofi-wayland,
+}:
+############
+# Packages #
+#######################################################################
+stdenv.mkDerivation (finalAttrs: {
+  pname = "backup-cli";
+  version = "release-2024.06.17-23.07.39";
+  src = fetchurl {
+    url = "https://github.com/RevoluNix/pkg-backup-cli/releases/download/release-2024.06.17-23.07.39/src-backup-cli.tar.gz";
+    sha256 = "101265ade361e0bbf8791826acf12667504419cf5e98acbb7ff4072ac8e04ae7";
+  }; 
+  # ----------------------------------------------------------------- #
+  nativeBuildInputs = [ makeWrapper ];
+  prePatch = ''
+    patchShebangs . ;
+  '';
+  # ----------------------------------------------------------------- #
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+
+    runHook postInstall
+  '';
+  # ----------------------------------------------------------------- #
+  postFixup = ''
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix PATH : ${lib.makeBinPath [ rofi-wayland ]}
+  '';
+  # ----------------------------------------------------------------- #
+  meta = {
+    description = "CLI backup/sync manager";
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.lgpl;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
+
