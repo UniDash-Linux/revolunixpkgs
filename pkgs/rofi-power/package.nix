f924a61ee424e31b5ce5583e6efad6f3eca633ca
Pikatsuto <pikatsuto@gmail.com>
2024-06-18 22:17:12 +0000

    feat: Update/Upload rofi-power package

--- pkgs/rofi-power/package.nix
+++ pkgs/rofi-power/package.nix
@@ -3,35 +3,33 @@
   stdenv,
   lib,
   makeWrapper,
-  bash,
+  rofi-wayland,
+  swaylock-effects,
 }:
 ############
 # Packages #
-#########################################################################
+#######################################################################
 let
   iconPath = "icon.png";
-  name = "Exemple Application";
-  comment = "Exemple Application";
+  name = "Rofi Power";
+  comment = "Rofi power menu";
 in
-# --------------------------------------------------------------------- #
+# ----------------------------------------------------------------- #
 stdenv.mkDerivation (finalAttrs: {
-  pname = "exemple";
-  version = "nightly-2024.06.18-22.13.45";
-  ## ----------------------------------------------------------------- ##
+  pname = "rofi-power";
+  version = "nightly-2024.06.18-22.17.09";
+  # ----------------------------------------------------------------- #
   src = fetchurl {
-    url = "https://github.com/RevoluNix/pkg-rofi-power/releases/download/nightly-2024.06.18-22.13.45/src-rofi-power.tar.gz";
-    sha256 = "cf5a419366b3793ff4d045367e3030cdf2f46fa9b506eb461f29160323ef02ef";
+    url = "https://github.com/RevoluNix/pkg-rofi-power/releases/download/nightly-2024.06.18-22.17.09/src-rofi-power.tar.gz";
+    sha256 = "0fea48b4028524a6baac6a253be7cdbc0a639b67287c14c25c2963c259419352";
   }; 
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   nativeBuildInputs = [ makeWrapper ];
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   prePatch = ''
     patchShebangs . ;
-
-    substituteInPlace exemple \
-      --replace-fail "exemple-2" "${placeholder "out"}/bin/exemple-2"
   '';
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   installPhase = ''
     runHook preInstall
 
@@ -39,7 +37,6 @@ stdenv.mkDerivation (finalAttrs: {
     cp -r ./ $out/Applications/${finalAttrs.pname}/
 
     install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
-    install -Dm 755 exemple-2 $out/bin/exemple-2
 
     echo -e "[Desktop Entry]\n" \
       "Type=Application\n" \
@@ -54,19 +51,16 @@ stdenv.mkDerivation (finalAttrs: {
 
     runHook postInstall
   '';
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   postFixup = ''
-    wrapProgram $out/bin/exemple-2 \
-      --prefix PATH : ${lib.makeBinPath [
-        bash
-      ]}
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix PATH : ${lib.makeBinPath [ rofi-wayland swaylock-effects ]}
   '';
-  ## ----------------------------------------------------------------- ##
+  # ----------------------------------------------------------------- #
   meta = {
     description = comment;
-    homepage = "https://github.com/RevoluNix/pkgs-template/";
     maintainers = with lib.maintainers; [ pikatsuto ];
-    licenses = lib.licenses.lgpl2;
+    licenses = lib.licenses.gpl3Plus;
     platforms = lib.platforms.linux;
     mainProgram = finalAttrs.pname;
   };
