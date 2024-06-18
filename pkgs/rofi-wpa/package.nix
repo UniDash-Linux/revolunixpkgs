498f570cc81704a04d915e69b1ea9595343612ee
Pikatsuto <pikatsuto@gmail.com>
2024-06-18 22:31:15 +0000

    feat: Update/Upload rofi-wpa package

---
+++ pkgs/rofi-wpa/package.nix
@@ -0,0 +1,74 @@
+{
+  fetchurl,
+  stdenv,
+  lib,
+  makeWrapper,
+  bash,
+}:
+############
+# Packages #
+#########################################################################
+let
+  iconPath = "icon.png";
+  name = "Exemple Application";
+  comment = "Exemple Application";
+in
+# --------------------------------------------------------------------- #
+stdenv.mkDerivation (finalAttrs: {
+  pname = "exemple";
+  version = "nightly-2024.06.18-22.31.13";
+  ## ----------------------------------------------------------------- ##
+  src = fetchurl {
+    url = "https://github.com/RevoluNix/pkg-rofi-wpa/releases/download/nightly-2024.06.18-22.31.13/src-rofi-wpa.tar.gz";
+    sha256 = "fb63eac81b9bc76d08ce20540f7c233dc7e4dc01c36d4630ee5c6870e68b7432";
+  }; 
+  ## ----------------------------------------------------------------- ##
+  nativeBuildInputs = [ makeWrapper ];
+  ## ----------------------------------------------------------------- ##
+  prePatch = ''
+    patchShebangs . ;
+
+    substituteInPlace exemple \
+      --replace-fail "exemple-2" "${placeholder "out"}/bin/exemple-2"
+  '';
+  ## ----------------------------------------------------------------- ##
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/ $out/Applications/
+    cp -r ./ $out/Applications/${finalAttrs.pname}/
+
+    install -Dm 755 ${finalAttrs.pname} $out/bin/${finalAttrs.pname}
+    install -Dm 755 exemple-2 $out/bin/exemple-2
+
+    echo -e "[Desktop Entry]\n" \
+      "Type=Application\n" \
+      "Name=${name}\n" \
+      "Comment=${comment}\n" \
+      "Icon=$out/Applications/${finalAttrs.pname}/${iconPath}\n" \
+      "Exec=$out/bin/${finalAttrs.pname}\n" \
+      "Terminal=false" > ./${finalAttrs.pname}.desktop
+
+    install -D ${finalAttrs.pname}.desktop \
+      $out/share/applications/${finalAttrs.pname}.desktop
+
+    runHook postInstall
+  '';
+  ## ----------------------------------------------------------------- ##
+  postFixup = ''
+    wrapProgram $out/bin/exemple-2 \
+      --prefix PATH : ${lib.makeBinPath [
+        bash
+      ]}
+  '';
+  ## ----------------------------------------------------------------- ##
+  meta = {
+    description = comment;
+    homepage = "https://github.com/RevoluNix/pkgs-template/";
+    maintainers = with lib.maintainers; [ pikatsuto ];
+    licenses = lib.licenses.lgpl2;
+    platforms = lib.platforms.linux;
+    mainProgram = finalAttrs.pname;
+  };
+  #######################################################################
+})
