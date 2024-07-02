ddb8e18d3bebe6c988557fd47e85cf1151e6aba5
Gabriel Guillou <pikatsuto@gmail.com>
2024-07-03 00:56:46 +0200

    feat: include nixospkgs with overlay (#4)

--- flake.nix
+++ flake.nix
@@ -1,40 +1,69 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     virtual-machines = {
       url = "github:RevoluNix/module-virtual-machines";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, virtual-machines }: let
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    virtual-machines,
+    ...
+  }: let
     defaultSystems = [
       "aarch64-linux"
       "aarch64-darwin"
       "x86_64-darwin"
       "x86_64-linux"
     ];
- 
+
+    system = "x86_64-linux";
+    pkgs-settings = {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
     forAllSystems = function:
       nixpkgs.lib.genAttrs defaultSystems
       (system: function nixpkgs.legacyPackages.${system});
-    in {
-      nixosModules = rec {
-        virtualMachines = virtual-machines.nixosModules.default;
-        default = virtualMachines;
-      };
 
-      overlays.default = (final: prev:
-        (self.packages."x86_64-linux"));
+    revoluNixOverlays = (final: prev:
+      (packages."${system}"));
 
-      packages = forAllSystems (pkgs:
-        let
-          scope = pkgs.lib.makeScope
-            pkgs.newScope (self: { inherit inputs; });
-        in
-        pkgs.lib.filesystem.packagesFromDirectoryRecursive {
-          inherit (scope) callPackage;
-          directory = ./pkgs;
-        });
+    packages = forAllSystems (pkgs:
+      let
+        scope = pkgs.lib.makeScope
+          pkgs.newScope (self: { inherit inputs; });
+      in
+      pkgs.lib.filesystem.packagesFromDirectoryRecursive {
+        inherit (scope) callPackage;
+        directory = ./pkgs;
+      });
+
+    revoluNixModules =  nixpkgs.nixosModules // {
+      virtualMachines = virtual-machines.nixosModules.default;
+    };
+    defaultModules = [];
+
+    in nixpkgs // {
+      revolunixpkgs = import nixpkgs (pkgs-settings // {
+        overlays = [
+          (_: _: {
+            nixpkgs = import nixpkgs pkgs-settings;
+            unstable = import nixpkgs-unstable pkgs-settings;
+            nixosModules = revoluNixModules;
+            inherit defaultModules;
+          })
+          revoluNixOverlays
+        ];
+      }); 
+      unstable = nixpkgs-unstable;
+      nixosModules = revoluNixModules;
+      inherit defaultModules;
     };
 }
