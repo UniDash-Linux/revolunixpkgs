241bbd3ab9a8d9fb778340415dd0d22b58bf7051
Pikatsuto <pikatsuto@gmail.com>
2024-09-11 10:00:44 +0200

    feat: add proxmox hypervisor

--- flake.nix
+++ flake.nix
@@ -17,6 +17,9 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    proxmox-nixos = {
+      url = "github:RevoluNix/proxmox-nixos";
+    };
   };
 
   outputs = inputs @ {
@@ -26,6 +29,7 @@
     virtual-machines,
     revolunixos,
     home-manager,
+    proxmox-nixos,
     ...
   }: let
 
@@ -68,25 +72,55 @@
 ###########
 # Overlay #
 #######################################################################
-    overlayContents = {
+    overlayModules = {
       nixosModules = nixpkgs.nixosModules // {
         virtualMachines = virtual-machines.nixosModules.default;
         home-manager = home-manager.nixosModules.home-manager;
+        proxmox-nixos = proxmox-nixos.nixosModules.proxmox-ve;
       };
+
       configsImports = {
         revolunixos = revolunixos.configsImports;
       };
+
       defaultModules = [
       ];
-
-      unstable = import unstable pkgsSettings;
-      purepkgs = nixpkgs;
     };
 
+    overlayPkgs = {
+      unstable = import unstable pkgsSettings;
+      stable = import nixpkgs pkgsSettings; 
+      purepkgs = nixpkgs;
+
+      inherit proxmoxPkgs;
+    };
+
+
+    proxmoxOverlays = [
+      (_: _: (packages."${system}"))
+      (_: _: overlayPkgs)
+      (self: super: {
+        # src: https://github.com/NixOS/nixpkgs/commit/7e94ac48e0c68bdc9d2b39e50e024e7170f83838
+        # issue/PR: https://github.com/NixOS/nixpkgs/pull/325059
+        ceph = super.ceph.overrideAttrs {
+          postPatch = ''
+            substituteInPlace cmake/modules/Finduring.cmake \
+              --replace-fail "liburing.a liburing" "uring"
+          '';
+        };
+      })
+      proxmox-nixos.overlays.${system}
+    ];
+
     revoluNixOverlays = [
-        (_: _: (packages."${system}"))
-        (_: _: overlayContents)
-      ];
+      (_: _: (packages."${system}"))
+      (_: _: overlayModules)
+      (_: _: overlayPkgs)
+    ];
+
+    proxmoxPkgs = import nixpkgs (pkgsSettings // {
+      overlays = proxmoxOverlays;
+    });
 
     revoluNixPkgs = import nixpkgs (pkgsSettings // {
       overlays = revoluNixOverlays;
